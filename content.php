6165c3c23b896c68059848dccbe5efccc79a5252
gbpandero <5133398-gbpandero@users.noreply.gitlab.com>
2020-07-19 16:53:30 -0500

    adding post with thumbnails

--- content.php
+++ content.php
@@ -3,10 +3,9 @@
 <?php 
 
 		if ( has_post_thumbnail() ){
-      echo '<h1>hay imagen</h1>'
-      the_post_thumbnail()
+      the_post_thumbnail();
     } else {
-      echo '<h1>No hay imagen</h1>'
+      echo '<h1>No hay imagen</h1>';
     }
   ?>
   </div>
