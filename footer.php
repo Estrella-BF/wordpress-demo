c58a58c2bad38714f8b86470a26e0877cced9b30
gbpandero <5133398-gbpandero@users.noreply.gitlab.com>
2020-07-19 14:59:25 -0500

    first commit

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer class="footer_id">
+<?php wp_footer(); ?>
+<style>
+.footer_all {
+  background: green;
+  padding: 10px;
+}
+</style>
+
+<div class="footer_all">
+<h3>Estamos en el footer</h3>
+</div>
+</footer>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<div class="all_content">
+<?php
+  if ( have_posts() ) {
+    if ( is_home() && ! is_front_page() ) {
+      while ( have_posts() ) {
+        the_post();
+      }
+    }
+  } else {
+    echo "sin post";
+  }
+?>
+</div>
+<?php get_footer(); ?>
